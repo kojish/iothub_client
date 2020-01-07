78f75374198757b57c213a046c5c9d63b5977663
Koji Shimizu <koji.shimizu@outlook.com>
2020-01-07 16:09:50 +0900

    Added DPS

--- csharp/IoTHubClient.cs
+++ csharp/IoTHubClient.cs
@@ -9,6 +9,8 @@ using Microsoft.Azure.Devices.Shared;
 using Microsoft.Azure.Devices.Client.Transport.Mqtt;
 using System.Net;
 using System.Web.Profile;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
 
 namespace IoTHubClient
 {
@@ -20,10 +22,18 @@ namespace IoTHubClient
         static string proxyUser = "";
         static string proxyPassword = "";
         // デバイスIDとホスト名はご使用の環境に合わせて変更ください：
-        static string deviceId = "mypc";    // デバイス名
         static DeviceClient deviceClient;
-        // 接続文字列
+        static string hubName = "";
+        static string deviceId = "";    // デバイス名
+        static string sharedAccessKey = "";
         static string connectionString = "HostName=iothubkssea.azure-devices.net;DeviceId=mypc;SharedAccessKey=fuPvLGhf0kxzK/woYCCZEOkKXy57bdS0Y6OW9kJVmoU=";
+        // For DPS
+        static string globalEndpoint = "global.azure-devices-provisioning.net";
+        static string scopeId = "0ne0006E3BA";
+        static string registrationId = "185a9487-6605-47b9-a630-f5a46f856b80";
+        static string primaryKey = "D8+xk9ETzqhplZJ7EoMrKbEp/70bLiIktUKD1gG/1Nc=";
+        static string secondaryKey = "D8+xk9ETzqhplZJ7EoMrKbEp/70bLiIktUKD1gG/1Nc=";
+        static DeviceRegistrationResult registrationResult = null;
 
         // Proxy settings
         private static WebProxy getProxySettings(string proxyUri, int port, string user, string password)
@@ -46,14 +56,41 @@ namespace IoTHubClient
                 }
             };
 
+            provisionDevice(scopeId, registrationId, primaryKey, secondaryKey).Wait();
+
+            if (registrationResult != null) {
+                connectionString = "HostName=" + registrationResult.AssignedHub + ";DeviceId=" + registrationResult.DeviceId + ";SharedAccessKey=" + primaryKey;
+            }
+            Console.WriteLine(connectionString);
+
             deviceClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
             deviceClient.SetConnectionStatusChangesHandler(ConnectionStatusChangeHandler);
             sendMessages();
-            //sendData();
+            //sendDataFromFile();
             receiveCloud2DeviceMessage();
             remoteMethod();
             deviceTwin();
             Console.ReadLine();
+        }
+
+        private static async Task provisionDevice(string scope_id, string registrationId, string primaryKey, string secondaryKey)
+        {
+            var security = new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey);
+            using (var transport = new ProvisioningTransportHandlerMqtt(TransportFallbackType.WebSocketOnly))
+            {
+                Console.WriteLine("ProvisioningClient strt");
+                ProvisioningDeviceClient provClient = ProvisioningDeviceClient.Create(globalEndpoint, scope_id, security, transport);
+                if (provClient == null) Console.Write("null 0");
+                Console.WriteLine("ProvisioningClient strt1: " + provClient.ProductInfo);
+               registrationResult = await provClient.RegisterAsync();
+                if (registrationResult == null) Console.WriteLine("null;;;");
+                else Console.WriteLine("ProvisioningClient strt2: " + registrationResult.Status);
+                if (registrationResult.Status == ProvisioningRegistrationStatusType.Assigned)
+                {
+                    Console.WriteLine("ProvisioningClient strt3: " + registrationResult.Status);
+                    Console.WriteLine($"ProvisioningClient AssignedHub: {registrationResult.AssignedHub}; DeviceID: {registrationResult.DeviceId}");
+                }
+            }
         }
 
         // IoT Hub とのコネクションステータスを受信する処理
@@ -63,11 +100,10 @@ namespace IoTHubClient
             Console.WriteLine("Connection Status Changed to {0}", status);
             Console.WriteLine("Connection Status Changed Reason is {0}", reason);
             Console.WriteLine();
-
         }
-        
-        // ローカルのCSVファイルをJSONに変換してから IoT Hub へ送信する処理
-        private static async void sendData()
+
+
+        private static async void sendDataFromFile()
         {
             string localFile = @"C:\tmp\TEST_CSV.CSV"; // 対象ファイル
             StreamReader reader = new StreamReader(localFile, System.Text.Encoding.GetEncoding("shift_jis"));
@@ -156,17 +192,19 @@ namespace IoTHubClient
         {
             while (true)
             {
-                int rand1 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(20, 50);
+                int rand1 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(25, 30);
                 Thread.Sleep(1000);
-                int rand2 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(15, 40);
+                int rand2 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(15, 25);
                 int rand3 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(1, 10);
+                int rand4 = new Random((int)DateTime.Now.Ticks & 0x0000FFFF).Next(30, 45);
 
                 var telemetryDataPoint = new
                 {
-                    deviceId = deviceId,
+                    deviceId = registrationResult.DeviceId,
                     pressure = rand1,
                     rotation = rand2,
                     oil_level = rand3,
+                    fan_mode = rand4,
                     event_time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
                 };
                 var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
